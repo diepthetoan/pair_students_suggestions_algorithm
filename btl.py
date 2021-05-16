66f876c95e1df34549f87e906f3b4059907bb04a
Diep Toan <diepthetoan1@gmail.com>
2021-05-16 17:25:07 +0700

    Add sort scores in weeks

--- btl.py
+++ btl.py
@@ -9,7 +9,12 @@ def sort_scores(arr): # arr=[2,9,4,5,1,8]
   # sort dictionary by value
   sorted_scores = {k: v for k, v in sorted(result.items(), key=lambda item: item[1])}
   return sorted_scores # result: {4: 1, 0: 2, 2: 4, 3: 5, 5: 8, 1: 9}
-  
+
+def sort_scores_in_weeks(arrs): # arrs=[[2,9,4,5,1,8], [5,1,9,2,6,7], [1,2,7,5,3,6]]
+  return [sort_scores(arr)
+            for arr in arrs]
+
+print(sort_scores_in_weeks([[2,9,4,5,1,8], [5,1,9,2,6,7], [1,2,7,5,3,6]]))
 
 # Bắt cặp học viên
 def pair_students(sorted_scored): # sorted_scored: {4: 1, 0: 2, 2: 4, 3: 5, 5: 8, 1: 9}
