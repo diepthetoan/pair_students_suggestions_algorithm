0851bbc444b9ee9ccac6d6c48d51622518defc68
hien.p <hien.p@autonomous.nyc>
2021-05-18 15:44:04 +0700

    add function

--- read.py
+++ read.py
@@ -1,4 +1,16 @@
 import pandas as pd
+import xlrd
+
+
+def read_file():
+    loc = 'C:\\workspace\\OneDrive - Robert Bosch GmbH\\03_Master\\01_HK1\\GiaiThuatNangCao\\pair_students_suggestions_algorithm-master\\week1.xlsx'
+    wb = xlrd.open_workbook(loc)
+    sheet = wb.sheet_by_index(0)
+    outData = []
+    for i in range(2, 11):
+        pointWeek = sheet.col_values(i)
+    outData.append(pointWeek)
+    return outData
 
 
 def read_file_csv(uri):
